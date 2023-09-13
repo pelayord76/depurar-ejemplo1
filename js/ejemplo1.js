33f0ab19de74710d61eca8667ace43b1d53189ad
David Fernandez <davidfga@educastur.org>
2023-09-13 16:39:26 +0200

    subida inicial

---
+++ js/ejemplo1.js
@@ -0,0 +1,27 @@
+/**
+ * DWEC: Ejemplo 1. Encontrar los errores del script
+ * @author Maria R. F.
+ * 
+ * Realiza un script que se encargue de leer 10 nombres por pantalla y los inserte en un array. 
+ * Posteriormente, los nombres se mostrarán una unica ventana emergente
+ */
+
+/* Encontrar los errores que tiene el siguiente script, usando las herramientas de depuración de Google Chrome */
+
+var nombres=[];
+
+var NUMERO_NOMBRES=10;
+
+
+
+
+/* AVISOS (WARNING) */
+console.warn("Mensaje warn");
+/* ERRORES */
+console.error("Mensaje err");
+/* INFORMACIÓN (LOG) */
+console.log("Mensaje log");
+/* DEPURACIÓN (DEBUG) */
+console.debug("Mensaje deb");
+ 
+
